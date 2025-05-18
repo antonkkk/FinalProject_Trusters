0d01dd3a78ef226bcfbb55a5bbff9148ea044fdb
KatyaGonchar <katyahonchar95@gmail.com>
2025-05-18 13:20:35 +0300

    fixed pylint

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
 import json
 import pytest
 from selenium import webdriver
-from helper.utils import send_request
 from selenium.webdriver.chrome.options import Options
+from helper.utils import send_request
 
 
 @pytest.fixture(scope="function")
